197f5043ed77fca0979d133b3f16b69461575097
aj124578 <aj124578@naver.com>
2023-02-15 12:26:12 +0900

    mapper 완료

--- src/main/java/shop/mtcoding/bankapp/model/user/User.java
+++ src/main/java/shop/mtcoding/bankapp/model/user/User.java
@@ -2,9 +2,7 @@ package shop.mtcoding.bankapp.model.user;
 
 import java.sql.Timestamp;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 
