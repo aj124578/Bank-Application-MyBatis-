d2aac4244d4f00dda0d410840a15688ca8b299b8
aj124578 <aj124578@naver.com>
2023-02-15 11:05:52 +0900

    모델링 완료 - table 작성 완료

--- src/main/resources/db/table.sql
+++ src/main/resources/db/table.sql
@@ -2,7 +2,25 @@ CREATE TABLE user_tb(
     id int auto_increment primary key,
     username varchar unique not null,
     password varchar not null,
-    email varchar not null,
-    profile varchar, 
+    fullname varchar not null,
+    created_at timestamp not null
+);
+
+CREATE TABLE account_tb(
+    id int auto_increment primary key,
+    number varchar unique not null,  -- 계좌번호
+    password varchar not null,         
+    balance bigint not null,         -- 잔액
+    user_id int,
+    created_at timestamp not null
+);
+
+CREATE TABLE history_tb(
+    id int auto_increment primary key,
+    amount bigint not null,          -- 거래금액
+    w_balance bigint,
+    d_balance bigint.
+    w_acoount int,                   -- 출금된 계좌
+    d_account int,                   -- 입금된 계좌
     created_at timestamp not null
 );
